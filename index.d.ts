0db9e328450a31c276394280395aaf9266e689fc
jlau <jlau@control-tec.com>
2017-12-21 10:28:03 -0500

    Fix Overwrite to be homomorphic.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 /**
  * The type of all values; nothing is known about it a priori
  * except that it exists. The same idea as Flow's `mixed` type.
- * 
+ *
  * @see https://github.com/Microsoft/TypeScript/issues/10715
  */
 export type unknown = {} | undefined | null;
@@ -9,7 +9,7 @@ export type unknown = {} | undefined | null;
 /**
  * Remove the variants of the second union of string literals from
  * the first.
- * 
+ *
  * @see https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
  */
 type Diff<T extends string, U extends string> = (
@@ -20,7 +20,7 @@ type Diff<T extends string, U extends string> = (
 
 /**
  * Drop keys `K` from `T`.
- * 
+ *
  * @see https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
  */
 export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
@@ -28,21 +28,21 @@ export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
 /**
  * Like `T & U`, but where there are overlapping properties using the
  * type from U only.
- * 
+ *
+ * @see https://github.com/pelotom/type-zoo/issues/2
  * @see https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
  */
-export type Overwrite<T, U> = { [P in Diff<keyof T, keyof U>]: T[P] } & U;
-
+export type Overwrite<T, U> = Omit<T, Diff<keyof T, Diff<keyof T, keyof U>>> & U;
 /**
  * `T` without the possibility of `undefined` or `null`.
- * 
+ *
  * @see https://github.com/Microsoft/TypeScript/issues/15012#issuecomment-346499713
  */
 export type NonNullable<T> = T & {};
 
 /**
  * Make all properties of `T` required and non-nullable.
- * 
+ *
  * @see https://github.com/Microsoft/TypeScript/issues/15012#issuecomment-346499713
  */
 export type Required<T> = {
