51a745b764c656cdfcea48bea5a4786873009688
UselessPickles <jlau@uselesspickles.com>
2018-01-28 11:27:44 -0500

    Simplify Overwrite by using new Overlap type.

--- index.d.ts
+++ index.d.ts
@@ -37,7 +37,7 @@ export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
  * @see https://github.com/pelotom/type-zoo/issues/2
  * @see https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
  */
-export type Overwrite<T, U> = Omit<T, Diff<keyof T, Diff<keyof T, keyof U>>> & U;
+export type Overwrite<T, U> = Omit<T, Overlap<keyof T, keyof U>> & U;
 /**
  * `T` without the possibility of `undefined` or `null`.
  *
